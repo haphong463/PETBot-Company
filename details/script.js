0fd0b86c7f24bc12a26aa8f1c2d32abc75d3fd23
haphong463 <111565442+haphong463@users.noreply.github.com>
2022-11-25 18:15:08 +0700

    a

---
+++ details/script.js
@@ -0,0 +1,21 @@
+
+const allHoverImages = document.querySelectorAll('.hover-container-1 div img');
+const imgContainer = document.querySelector('.img-container-1-1');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('click', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img) => {
+        img.parentElement.classList.remove('active');
+    });
+}
